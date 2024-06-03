dbf9ab1175ade592cd15c3afb188b1ae7e414347
Carminelm <carminelm54@gmail.com>
2024-06-03 15:45:14 +0200

    add user dates

---
+++ main.js
@@ -0,0 +1,14 @@
+//dati
+const userName = document.getElementById ('name');
+const userSurname = document.getElementById ('surname');
+const userColor = document.getElementById ('color');
+const userNumber = document.getElementById ('number');
+
+let userDates = document.getElementById ('results');
+
+//numero
+
+userNumber.innerHTML = 21;
+
+
+
